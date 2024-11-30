abe7673148019f0b89da76323674cc1ba9ef0e30
paulsaji200 <paulsaji1791@gmail.com>
2024-11-30 09:33:18 +0530

    sea1248889

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -14,14 +14,14 @@ const Admin = () => {
       {/* Top navigation */}
       <AdminNavbar />
 
-      {/* Sidebar and Outlet in a flex row */}
+     
       <div className="flex flex-1">
-        {/* Sidebar */}
+       
         <div className="w-1/5 bg-gray-200">
           <Sidebar />
         </div>
 
-        {/* Outlet / Main Content */}
+      
         <div className="flex-1 bg-white overflow-y-auto">
           <Outlet />
         </div>
