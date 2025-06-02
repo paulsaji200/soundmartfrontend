90928ee42d91f6ea50c52161649f5f1611363bc2
Paul Saji <164215907+paulsaji200@users.noreply.github.com>
2025-06-02 18:44:43 +0530

    Update axios.js
    ....

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://soundmartserver.onrender.com",
+  baseURL: "https://soundmartserver.onrender.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
